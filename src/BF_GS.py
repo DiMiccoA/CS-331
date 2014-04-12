a3842925dc16349d3be4e3e7089b8fab2dcb98ea
DiMiccoA <antonio@scottdimicco.com>
2014-04-12 10:49:30 -0700

    Created Project 1
    Setup Project 1 and created the main file and the bread-first
    graph-search python file

---
+++ src/BF_GS.py
@@ -0,0 +1,6 @@
+'''
+Created on Apr 12, 2014
+
+@author: anton_000
+'''
+
---
+++ src/main.py
@@ -0,0 +1,10 @@
+'''
+Created on Apr 12, 2014
+
+@author: anton_000
+'''
+
+def main():
+    random = 5
+    #do stuff here
+    
