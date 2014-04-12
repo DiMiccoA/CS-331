df1642e8682880b44f4ac08996284a7398829c2f
patrickoliver <oliverpa@onid.oregonstate.edu>
2014-04-12 14:24:50 -0700

    Added start state, goal state, and print
    Created a way to print a list of lists. But this can probably be
    replaced by something that just prints row by row as it chugs along the
    algorithm, if that is preferred.

--- src/main.py
+++ src/main.py
@@ -3,8 +3,23 @@ Created on Apr 12, 2014
 
 @author: anton_000
 '''
-
+def Print_Double_Array(arr):
+	#Prints a double array all pretty like.
+	for row in arr:
+		print('\t'.join(map(str, row)))
+	
 def main():
-    random = 5
+	a = [7, 2, 4]
+	b = [5, 0, 6]
+	c = [8, 3, 1]
+	start = [a, b, c]
+	x = [0, 1, 2]
+	y = [3, 4, 5]
+	z = [6, 7, 8]
+	goal = [x, y, z]
+	
+	Print_Double_Array(arr)
     #do stuff here
     
+if __name__ == '__main__':
+	main()
