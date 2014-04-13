6ae391b98b66bf673e94fd1fdc33b00e6ffd80aa
patrickoliver <oliverpa@onid.oregonstate.edu>
2014-04-12 20:58:50 -0700

    Added more functionality
    Changed create_array to create_state and made it make a string instead
    of an array. Added a print function to print the string.

--- src/BF_GS.py
+++ src/BF_GS.py
@@ -3,4 +3,10 @@ Created on Apr 12, 2014
 
 @author: anton_000
 '''
-
+def expand(state):
+	pass
+	
+def breadth_first(start):
+	closed = []
+	fringe = create_fringe(start)
+	
--- src/main.py
+++ src/main.py
@@ -4,15 +4,20 @@ Created on Apr 12, 2014
 @author: anton_000
 '''
 
-def build_array(file):
+def build_state(file):
 	#Builds a 2D array given a file
-	arr = []
+	#arr = []
+	#fd = open(file, 'r')
+	#for line in fd:
+	#	arr.append(line.strip().split(','))
+	state = ''
 	fd = open(file, 'r')
 	for line in fd:
-		arr.append(line.strip().split(','))
-
-	#print('\t'.join(arr[0]))
-	return arr
+		line = line.replace(',','').replace('\n','')
+		state = state+line
+		
+	return state
+	#return arr
 		
 	
 def print_double_array(arr):
@@ -21,28 +26,27 @@ def print_double_array(arr):
 		for row in element:
 			print('\t'.join(row))
 	
+def print_state(state):
+	for x in range(0,3):
+		print state[x*3]+' '+state[x*3+1]+' '+state[x*3+2]+'\n'
+		
 def main():
-	start = []
-	goal = []
-	start.append(build_array('testStart1.txt'))
-	goal.append(build_array('testGoal1.txt'))
-	print 'start config:'
-	print_double_array(start)
-	print '\ngoal config:'
-	print_double_array(goal)
-	
+	start = ''
+	goal = ''
+	start = build_state('testStart1.txt')
+	print_state(start)
+	goal = build_state('testGoal1.txt')
+	print '\n'
+	print_state(goal)
 
-#	a = [7, 2, 4]
-#	b = [5, 0, 6]
-#	c = [8, 3, 1]
-#	start = [a, b, c]
-#	x = [0, 1, 2]
-#	y = [3, 4, 5]
-#	z = [6, 7, 8]
-#	goal = [x, y, z]
-	
-#	Print_Double_Array(arr)
-    #do stuff here
-    
+#	start = []
+#	goal = []
+#	start.append(build_array('testStart1.txt'))
+#	goal.append(build_array('testGoal1.txt'))
+#	print 'start config:'
+#	print_double_array(start)
+#	print '\ngoal config:'
+#	print_double_array(goal)
+   
 if __name__ == '__main__':
 	main()
