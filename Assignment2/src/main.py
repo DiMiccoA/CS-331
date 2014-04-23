17e2f9860e42de7db0ec2fbbc3a572a0176e80d2
DiMiccoA <antonio@scottdimicco.com>
2014-04-22 18:38:18 -0700

    Setup Rand Player
    Now it should work for both human and rand players in either order.

--- Assignment2/src/main.py
+++ Assignment2/src/main.py
@@ -19,10 +19,62 @@ class main:
         self.canvas.create_rectangle(100,300,200,0, outline="black")
         self.canvas.create_rectangle(0,100,300,200, outline="black")
         
-#    def randplayer(self):
+    def is_empty(self, row, col):
+        if self.canvas.find_enclosed(row,col,row+100,col+100)==():
+            return 0
+        else:
+            return 1
         
-   
-    def start1(self):
+    def rand_player(self):
+        row = randint(0,2)*100
+        col = randint(0,2)*100
+        while self.is_empty(row, col):
+            row = randint(0,2)*100
+            col = randint(0,2)*100
+        if self.i%2==1:
+            X=(2*row+100)/2
+            Y=(2*col+100)/2
+            X1=int(row/100)
+            Y1=int(col/100)
+            self.canvas.create_oval( X+25, Y+25, X-25, Y-25, width=4, outline="blue")
+            self.TTT[Y1][X1]=1
+            self.i+=1
+        else:                         
+            X=(2*row+100)/2
+            Y=(2*col+100)/2
+            X1=int(row/100)
+            Y1=int(col/100)
+            self.canvas. create_line( X+20, Y+20, X-20, Y-20, width=4, fill="red")
+            self.canvas. create_line( X-20, Y+20, X+20, Y-20, width=4, fill="red")
+            self.TTT[Y1][X1]=9
+            self.i+=1
+        #self.check()
+        
+    def human(self,event):
+        for row in range(0,300,100):
+            for col in range(0,300,100):
+                if event.x in range(row,row+100) and event.y in range(col,col+100):
+                    if self.canvas.find_enclosed(row,col,row+100,col+100)==():
+                        if self.i%2==1:
+                            X=(2*row+100)/2
+                            Y=(2*col+100)/2
+                            X1=int(row/100)
+                            Y1=int(col/100)
+                            self.canvas.create_oval( X+25, Y+25, X-25, Y-25, width=4, outline="blue")
+                            self.TTT[Y1][X1]=1
+                            self.i+=1
+                        else:                         
+                            X=(2*row+100)/2
+                            Y=(2*col+100)/2
+                            X1=int(row/100)
+                            Y1=int(col/100)
+                            self.canvas. create_line( X+20, Y+20, X-20, Y-20, width=4, fill="red")
+                            self.canvas. create_line( X-20, Y+20, X+20, Y-20, width=4, fill="red")
+                            self.TTT[Y1][X1]=9
+                            self.i+=1
+        #self.check()
+        
+    def start(self):
         self.canvas.delete(ALL)
         self.label['text']=('Tic Tac Toe Game\n ' + self.player1 + ' vs ' + self.player2)
         self.canvas.bind("<ButtonPress-1>", self.play)  
@@ -31,75 +83,78 @@ class main:
         self.j=False
         
     def play(self,event):
-        for k in range(0,300,100):
-            for j in range(0,300,100):
-                if event.x in range(k,k+100) and event.y in range(j,j+100):
-                    if self.canvas.find_enclosed(k,j,k+100,j+100)==():
-                        if self.i%2==0:
-                            X=(2*k+100)/2
-                            Y=(2*j+100)/2
-                            X1=int(k/100)
-                            Y1=int(j/100)
-                            self.canvas.create_oval( X+25, Y+25, X-25, Y-25, width=4, outline="blue")
-                            self.TTT[Y1][X1]=1
-                            self.i+=1
-                        else:                         
-                            X=(2*k+100)/2
-                            Y=(2*j+100)/2
-                            X1=int(k/100)
-                            Y1=int(j/100)
-                            self.canvas. create_line( X+20, Y+20, X-20, Y-20, width=4, fill="red")
-                            self.canvas. create_line( X-20, Y+20, X+20, Y-20, width=4, fill="red")
-                            self.TTT[Y1][X1]=9
-                            self.i+=1
+        if self.player1 == "human":
+            self.human(event)
+            self.check()
+            if self.i < 9:
+                if self.player1 == "human":
+                    self.human(event)
+                elif self.player2 == "random":
+                    self.rand_player()
+                else:
+                    self.min_max_player()
+        elif self.player1 == "random":
+            self.rand_player()
+            self.check()
+            if self.i < 9:
+                if self.player1 == "human":
+                    self.human(event)
+                elif self.player2 == "random":
+                    self.rand_player()
+                else:
+                    self.min_max_player()
+        else:
+            self.min_max_player
+            self.check()
+            if self.i < 9:
+                if self.player1 == "human":
+                    self.human(event)
+                elif self.player2 == "random":
+                    self.rand_player()
+                else:
+                    self.min_max_player()
         self.check()
-        print "Here"
-        if self.player2 == "random":
-            self.randplayer()
         
     def check(self):
         #horizontal check
         for i in range(0,3):
-            if sum(self.TTT[i])==27:
+            if sum(self.TTT[i])==3:
                 self.label['text']=('2nd player wins!')
                 self.end()
-            if sum(self.TTT[i])==3:
+            elif sum(self.TTT[i])==27:
                 self.label['text']=('1st player wins!')
                 self.end()
                 
         #vertical check
         #the matrix below transposes self.TTT so that it could use the sum fucntion again
-        self.ttt=[[row[i] for row in self.TTT] for i in range(3)]
+        self.TTT=[[row[i] for row in self.TTT] for i in range(3)]
         for i in range(0,3):            
-            if sum(self.ttt[i])==27:
+            if sum(self.TTT[i])==3:
                 self.label['text']=('2nd player wins!')
                 self.end()
-            if sum(self.ttt[i])==3:
+            elif sum(self.TTT[i])==27:
                 self.label['text']=('1st player wins!')
                 self.end()
                 
         #check for diagonal wins
-        if self.TTT[1][1]==9:
+        if self.TTT[1][1]==1:
             if self.TTT[0][0]==self.TTT[1][1] and self.TTT[2][2]==self.TTT[1][1] :
                 self.label['text']=('2nd player wins!')
                 self.end()
-            if self.TTT[0][2]==self.TTT[1][1] and self.TTT[2][0]==self.TTT[1][1] :
+            elif self.TTT[0][2]==self.TTT[1][1] and self.TTT[2][0]==self.TTT[1][1] :
                 self.label['text']=('2nd player wins!')
                 self.end()
-        if self.TTT[1][1]==1:
+        elif self.TTT[1][1]==9:
             if self.TTT[0][0]==self.TTT[1][1] and self.TTT[2][2]==self.TTT[1][1] :
                 self.label['text']=('1st player wins!')
                 self.end()
-            if self.TTT[0][2]==self.TTT[1][1] and self.TTT[2][0]==self.TTT[1][1] :
+            elif self.TTT[0][2]==self.TTT[1][1] and self.TTT[2][0]==self.TTT[1][1] :
                 self.label['text']=('1st player wins!')
                 self.end()
                 
         #check for draws
         if self.j==False:
-            a=0
-            for i in range(0,3):
-                a+= sum(self.TTT[i])
-            if a==41:
+            if self.i==9:
                 self.label['text']=("It's a draw!")
                 self.end()
                 
