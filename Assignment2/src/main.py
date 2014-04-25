631b29d296db2efee80a5e9741f5e5256b11b644
DiMiccoA <antonio@scottdimicco.com>
2014-04-25 10:31:34 -0700

    Setup Everything but MinMax
    Almost everything works in the program except minmax hasn't been setup
    yet. There are still a few bugs too.

--- Assignment2/src/main.py
+++ Assignment2/src/main.py
@@ -24,6 +24,55 @@ class main:
             return 0
         else:
             return 1
+    
+    def utility(self, state):
+        winner = ""
+        #horizontal check
+        for i in range(0,3):
+            if sum(self.state[i])==3:
+                winner = "player2"
+            elif sum(self.state[i])==27:
+                winner = "player1"
+                    
+        #vertical check
+        if winner == "":
+            for i in range(0,3):    
+                if (self.state[0][i]+self.state[1][i]+self.state[2][i])==3:
+                    winner = "player2"
+                elif (self.state[0][i]+self.state[1][i]+self.state[2][i])==27:
+                    winner = "player1"
+                    
+        #check for diagonal wins
+        if winner == "":
+            if self.state[1][1]==1:
+                if self.state[0][0]==self.state[1][1] and self.state[2][2]==self.state[1][1] :
+                    winner = "player2"
+                elif self.state[0][2]==self.state[1][1] and self.state[2][0]==self.state[1][1] :
+                    winner = "player2"
+            elif self.state[1][1]==9:
+                if self.state[0][0]==self.state[1][1] and self.state[2][2]==self.state[1][1] :
+                    winner = "player1"
+                elif self.state[0][2]==self.state[1][1] and self.state[2][0]==self.state[1][1] :
+                    winner = "player1"
+                    
+        #check for draws
+        if winner == "":
+            if self.i==9:
+                winner = "draw"
+                    
+        #checks who won and returns value
+        if winner == "player1":
+            if (self.i%2) == 0:
+                return 1
+            else:
+                return (-1)
+        elif winner == "player2":
+            if (self.i%2) == 1:
+                return 1
+            else:
+                return (-1)
+        else:
+            return 0    #represents a draw
         
     def rand_player(self):
         row = randint(0,2)*100
@@ -48,7 +97,6 @@ class main:
             self.canvas. create_line( X-20, Y+20, X+20, Y-20, width=4, fill="red")
             self.TTT[Y1][X1]=9
             self.i+=1
-        #self.check()
         
     def human(self,event):
         for row in range(0,300,100):
@@ -72,52 +120,93 @@ class main:
                             self.canvas. create_line( X-20, Y+20, X+20, Y-20, width=4, fill="red")
                             self.TTT[Y1][X1]=9
                             self.i+=1
-        #self.check()
+                            
+    def play_hvh(self, event):
+        self.human(event)
+        self.check()
+            
+    def play_hvr(self, event):
+        self.human(event)
+        self.check()
+        if self.i < 9 and self.j == False:
+            self.rand_player()
+            self.check()
         
     def start(self):
         self.canvas.delete(ALL)
         self.label['text']=('Tic Tac Toe Game\n ' + self.player1 + ' vs ' + self.player2)
-        self.canvas.bind("<ButtonPress-1>", self.play)  
         self._board()
+        self.TTT=[[0,0,0],[0,0,0],[0,0,0]]
         self.i=0
         self.j=False
         
+        if self.player1 == "human":
+            if self.player2 == "human":
+                self.canvas.bind("<ButtonPress-1>", self.play_hvh)
+            elif self.player2 == "random":
+                self.canvas.bind("<ButtonPress-1>", self.play_hvr)
+        elif self.player1 == "random" and self.player2 == "human":
+            self.rand_player()
+            print "i = " + str(self.i)
+            self.canvas.bind("<ButtonPress-1>", self.play_hvr)
+        elif self.player1 == "minmax" and self.player2 == "human":
+            #self.min_max()
+            self.canvas.bind("<ButtonPress-1>", self.play)
+        else:
+            self.play()
+        
+    def end(self):
+        self.canvas.unbind("<ButtonPress-1>")
+        self.j=True
+    
     def play(self,event):
         if self.player1 == "human":
+            #print "human"
             self.human(event)
             self.check()
+            #print "check1"
             if self.i < 9:
-                if self.player1 == "human":
+                if self.player2 == "human":
                     self.human(event)
                 elif self.player2 == "random":
+                    #print "random"
                     self.rand_player()
                 else:
                     self.min_max_player()
+                self.check()
+                #print "check2"
         elif self.player1 == "random":
+            print "random " + str(self.i)
             self.rand_player()
             self.check()
             if self.i < 9:
-                if self.player1 == "human":
+                if self.player2 == "human":
+                    print "human " + str(self.i)
                     self.human(event)
                 elif self.player2 == "random":
                     self.rand_player()
                 else:
                     self.min_max_player()
+                self.check()
+                #print "check2"
         else:
             self.min_max_player
             self.check()
             if self.i < 9:
-                if self.player1 == "human":
+                if self.player2 == "human":
                     self.human(event)
                 elif self.player2 == "random":
                     self.rand_player()
                 else:
                     self.min_max_player()
-        self.check()
+                self.check()
+                #print "check2"
+#        Check values of horizontal and vertical rows here
         
     def check(self):
         #horizontal check
         for i in range(0,3):
+            #print "horizontal " + str(sum(self.TTT[i]))
             if sum(self.TTT[i])==3:
                 self.label['text']=('2nd player wins!')
                 self.end()
@@ -126,13 +215,14 @@ class main:
                 self.end()
                 
         #vertical check
-        #the matrix below transposes self.TTT so that it could use the sum fucntion again
-        self.TTT=[[row[i] for row in self.TTT] for i in range(3)]
-        for i in range(0,3):            
-            if sum(self.TTT[i])==3:
+        #the matrix below transposes self.TTT so that it could use the sum function again
+        #self.TTT=[[row[i] for row in self.TTT] for i in range(3)]
+        for i in range(0,3):  
+            #print "vertical " + str(self.TTT[0][i]+self.TTT[1][i]+self.TTT[2][i])     
+            if (self.TTT[0][i]+self.TTT[1][i]+self.TTT[2][i])==3:
                 self.label['text']=('2nd player wins!')
                 self.end()
-            elif sum(self.TTT[i])==27:
+            elif (self.TTT[0][i]+self.TTT[1][i]+self.TTT[2][i])==27:
                 self.label['text']=('1st player wins!')
                 self.end()
                 
@@ -157,10 +247,6 @@ class main:
             if self.i==9:
                 self.label['text']=("It's a draw!")
                 self.end()
-                
-    def end(self):
-        self.canvas.unbind("<ButtonPress-1>")
-        self.j=True
      
     def __init__(self, master, player1, player2):
         self.frame = Frame(master)
@@ -171,12 +257,10 @@ class main:
         self.label.pack(fill="both", expand=True)
         self.frameb=Frame(self.frame)
         self.frameb.pack(fill="both", expand=True)
-        self.TTT=[[0,0,0],[0,0,0],[0,0,0]]
         self.player1 = player1
         self.player2 = player2
         
-        if self.player1 == "human":
-            self.start1()
+        self.start()
         #self.Start1=Button(self.frameb, text='Click here to start\ndouble player', height=4, command=self.start1,bg='white', fg='purple')
         #self.Start1.pack(fill="both", expand=True, side=RIGHT)
         #self.Start2=Button(self.frameb, text='Click here to start\nsingle player', height=4, command=self.start2,bg='purple', fg='white')
